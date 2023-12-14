187282d53f71d0f64522865c8103445aae50a366
Dmytro Minaiev <dm.minaev03@gmail.com>
2023-12-15 00:13:26 +0200

    init

---
+++ wp-content/uploads/custom-css-js/37384.js
@@ -0,0 +1,8 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */ 
+
+map.addSource('properties', {
+cluster: true,                 clusterMaxZoom: 20, // Max zoom to cluster points on clusterRadius: 40 // Radius of each cluster when clustering points (defaults to 50)
+            }</script>
+<!-- end Simple Custom CSS and JS -->
