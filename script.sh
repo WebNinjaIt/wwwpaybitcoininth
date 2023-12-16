6ce695e420e09f17dff0d79b326d301204f0bf50
Dmytro Minaiev <dm.minaev03@gmail.com>
2023-12-16 18:06:12 +0200

    Добавление измененных файлов из группы script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,27 @@
-git status --short | head -n 15000 | awk '{print $2}' | while read file; do
-  git add -- "$file"
-done
+# Путь к вашему репозиторию
+repositoryPath="C:\Users\d.minaiev\source\wwwpaybitcoininth"
 
-git commit -m "Init from file bash"
+# Перейти в директорию репозитория
+cd "$repositoryPath"
+
+# Максимальное количество файлов для добавления в один коммит
+maxFilesPerCommit=30000
+
+# Получить список локально измененных файлов в репозитории
+modifiedFiles=$(git status --porcelain | grep '^ M' | cut -c4-)
+
+# Разделить измененные файлы на группы для коммитов
+commitGroups=$(echo "$modifiedFiles" | xargs -n $maxFilesPerCommit)
+
+# Пройти по группам и создать коммиты
+for group in $commitGroups; do
+    # Вывести информацию о процессе
+    echo "Добавление измененных файлов в Git и создание коммитов..."
+
+    # Добавить изменения и создать коммит
+    git add -- $group
+    git commit -m "Добавление измененных файлов из группы $group"
+
+    # Вывести информацию о завершении процесса
+    echo "Процесс для группы измененных файлов $group завершен."
+done
